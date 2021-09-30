9987c6c69e60de04fe56e838423211013633b85a
uvsq22109341 <gasedjal@gmail.com>
2021-09-30 14:21:36 +0200

    init 3

---
+++ fraction.java
@@ -0,0 +1,13 @@
+
+public class fraction {
+     private float a ; 
+     public  fraction(float a)
+     { 
+         this.a = a;
+     }
+
+     public float geta(){
+         return a ;
+     }
+
+}
---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args){
+     fraction f = new fraction(5) ;
+     System.out.println(f.geta() + "est le nombre") ;
+    }
+    }
