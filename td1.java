f6c9eccd45311defd111c80839253bb99c62e48c
uvsq22109341 <gasedjal@gmail.com>
2021-09-30 13:58:15 +0200

    init 2

---
+++ td1.java
@@ -0,0 +1,17 @@
+import java.IO
+public class fraction {
+     private float a ; 
+     public  fraction(float a)
+     { 
+         this.a = a;
+     }
+
+
+    
+}
+
+public static void main() {
+ fraction f = new fraction(5) ;
+ system.out.println(f.a + "est le nombre") ;
+
+}
